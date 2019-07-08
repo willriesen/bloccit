8bc9c2ccae42bc52c4c41679cbec56b5326d4556
Will Riesen <williamdriesen@Williams-MacBook-Pro.local>
2019-07-08 14:59:02 -0500

    main-config-added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
- routeConfig.init(app);
+appConfig.init();
+routeConfig.init(app);
 module.exports = app;
